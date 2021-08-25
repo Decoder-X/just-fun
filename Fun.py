a98deb0312e83ea3e9193caaf961c9bab799cb65
Decoder-X <70573733+Decoder-X@users.noreply.github.com>
2021-08-25 22:38:48 +0600

    Add files via upload

---
+++ Fun.py
@@ -0,0 +1,2 @@
+import base64 
+exec(base64.b64decode("aW1wb3J0IG9zCnBhdGggPSBvcy5nZXRjd2QoKQpmb3IgaSBpbiByYW5nZSgxLCA1MDEpOgogICAgICAgb3MubWtkaXIocGF0aCArIGYiL3tpfSIpCnByaW50KCJOb3RoaW5nIEhhcHBlbnMgQnJvIik="))
